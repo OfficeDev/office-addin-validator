632c95916696bd186d642ac26dde17ff15772105
beth-panx <beth_panx@hotmail.com>
2017-02-13 11:34:33 -0800

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+ * See LICENSE in the project root for license information.
+ */
+
+console.log("app has been started");
