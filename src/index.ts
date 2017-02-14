f2b1850fda1e92200dda58d53c4cf58bcc9de7c1
beth-panx <beth_panx@hotmail.com>
2017-02-14 13:41:24 -0800

    Create CLI. Validate cURL call.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,35 @@
+#!/usr/bin/env node
+
 /*
  * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
  * See LICENSE in the project root for license information.
  */
 
-console.log("app has been started");
+import * as commander from 'commander';
+
+let curl = require('curlrequest');
+let options = {
+  url: 'https://verificationservice.osi.office.net/ova/addincheckingagent.svc/api/addincheck?lang=zh-CH',
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/xml'
+  },
+  data: null,
+  include: true
+};
+
+commander
+  .arguments('<manifest>')
+  .option('-l, --language [optional]', 'localization language')
+  .action((manifest) => {
+    options.data = '@' + manifest;
+
+    curl.request(options, (err, data) => {
+      console.log('processing');
+      console.log(data);
+      console.log(err);
+    });
+  })
+  .parse(process.argv);
+
+
